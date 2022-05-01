de1c94c326ea0d7f14ab50782ba123caecfe76e3
jykim111 <jykim1@email.unc.edu>
2022-05-01 00:17:27 -0400

    Added variable definitions

--- src/App.js
+++ src/App.js
@@ -40,6 +40,19 @@ function App() {
     // 1. Create other variables for assignments, exams, and engagement here
     let currentA00 = document.querySelector('#a00').value
     let currentA01 = document.querySelector('#a01').value
+    let currentA02 = document.querySelector('#a02').value
+    let currentA03 = document.querySelector('#a03').value
+    let currentA04 = document.querySelector('#a04').value
+    let currentA05 = document.querySelector('#a05').value
+
+    let currentMidterm = document.querySelector('#midterm').value
+    let currentCommits = document.querySelector('#commits').value
+
+    let currentE01 = document.querySelector('#e01').value
+    let currentE02 = document.querySelector('#e02').value
+
+    let currentFinal = document.querySelector('#finals').value
+    let currentProject = document.querySelector('#project').value
     if (currentSum === '')
       return;
     // 2. This line is where you add the points all together. Add your other variable references here. Make sure that they match what is above and what is below in the HTML form
